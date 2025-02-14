0ae50e0077b4c03f8c81b73bb102f20ce8684aea
admin <polepalle.07@gmail.com>
2025-02-14 22:35:06 +0530

    Pushing terrafrom code

--- vm/main.tf
+++ vm/main.tf
@@ -83,8 +83,8 @@ resource "azurerm_virtual_machine" "main" {
   }
   os_profile {
     computer_name  = var.component
-    admin_username = "admin"
-    admin_password = "admin123"
+    admin_username = "testuser"
+    admin_password = "test1234!"
   }
   os_profile_linux_config {
     disable_password_authentication = false
@@ -103,8 +103,8 @@ resource "null_resource" "ansible" {
 
     connection {
       type     = "ssh"
-      user     = "admin"
-      password = "admin123"
+      user     = "testuser"
+      password = "test1234!"
       host     = azurerm_public_ip.main.ip_address
     }
 
